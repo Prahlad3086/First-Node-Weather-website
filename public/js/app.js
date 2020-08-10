db307a2066020401c1594e9d2e45d45846a8f3bf
PrahladKumar1316 <prahladkumar1316@gmail.com>
2020-08-10 20:09:23 +0530

    Remove unneccessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,3 @@
-console.log("Client Side Javascript Added!!")
-
-
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
