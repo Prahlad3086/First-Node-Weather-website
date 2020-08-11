573930512b00c4d59488591c4442e3380b43bfc5
PrahladKumar1316 <prahladkumar1316@gmail.com>
2020-08-11 14:08:44 +0530

    User Interface Changed

--- src/app.js
+++ src/app.js
@@ -36,7 +36,7 @@ app.get('/about', (req,res) =>{
 
 app.get('/help', (req,res) =>{
     res.render('help',{
-        title: 'Help Page',
+        title: 'Help',
         name: 'Prahlad Kumar'
     })
 })
